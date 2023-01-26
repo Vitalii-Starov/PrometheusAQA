937fe992f08be4a7458b27c705e0997ad269db57
Vitalii-Starov <vitalii86starov@gmail.com>
2023-01-26 20:18:41 +0100

    Add updated tests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,2 +1,29 @@
-def test_check_math():
-    assert 7 * 7 == 49
+import pytest
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = "Vitalii"
+        self.second_name = "Starov"
+
+
+@pytest.fixture
+def user():
+    yield User()
+
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == "Vitalii"
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Starov"
